b8f0c90b03e27789716dd4f22f734912085bd31c
Alex Rundquist <alrund@gmail.com>
2022-02-05 19:20:36 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,76 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+
+	ErrThisIsNotAReadableFile = errors.New("this is not a readable file")
+	ErrThisIsNotARegularFile  = errors.New("this is not a regular file")
+	ErrCreatedFile            = errors.New("unable to create file")
+	ErrSeekFile               = errors.New("unable to set the offset")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	size, err := FileSize(fromPath)
+	if err != nil {
+		fmt.Println(err)
+		return ErrUnsupportedFile
+	}
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+	if limit > size || limit == 0 {
+		limit = size
+	}
+
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return ErrThisIsNotAReadableFile
+	}
+
+	_, err = fromFile.Seek(offset, 0)
+	if err != nil {
+		return ErrSeekFile
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return ErrCreatedFile
+	}
+
+	return CopyWithBar(toFile, fromFile, limit)
+}
+
+func CopyWithBar(toFile, fromFile *os.File, limit int64) error {
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(io.LimitReader(fromFile, limit))
+	_, err := io.Copy(toFile, barReader)
+	if err != nil {
+		return err
+	}
+	bar.Finish()
+
 	return nil
+}
+
+func FileSize(path string) (size int64, err error) {
+	file, err := os.Stat(path)
+	if err != nil {
+		return 0, err
+	}
+
+	mode := file.Mode()
+	if !mode.IsRegular() {
+		return 0, ErrThisIsNotARegularFile
+	}
+
+	return file.Size(), err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,113 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const ToFile = "/tmp/xxx"
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("get file size", func(t *testing.T) {
+		from := "testdata/input.txt"
+		var expectedSize int64 = 6617
+		fromSize, err := FileSize(from)
+
+		require.Nil(t, err)
+		require.Equal(t, expectedSize, fromSize)
+	})
+
+	t.Run("get not exists file size", func(t *testing.T) {
+		from := "not_exists_file"
+		_, err := FileSize(from)
+		require.Error(t, err)
+	})
+
+	t.Run("copy file", func(t *testing.T) {
+		from := "testdata/input.txt"
+		var offset, limit int64
+		err := Copy(from, ToFile, offset, limit)
+		require.Nil(t, err)
+
+		fromFile, _ := os.Stat(from)
+		fromSize := fromFile.Size()
+
+		toFile, _ := os.Stat(ToFile)
+		toSize := toFile.Size()
+
+		require.Equal(t, fromSize, toSize)
+	})
+
+	t.Run("copy a file with limit exceeding the file size", func(t *testing.T) {
+		from := "testdata/out_offset0_limit10000.txt"
+		var offset int64
+		var limit int64 = 10000
+		err := Copy(from, ToFile, offset, limit)
+		require.Nil(t, err)
+
+		fromFile, _ := os.Stat(from)
+		fromSize := fromFile.Size()
+
+		toFile, _ := os.Stat(ToFile)
+		toSize := toFile.Size()
+
+		require.Equal(t, fromSize, toSize)
+	})
+
+	t.Run("copy a file with offset exceeding the file size", func(t *testing.T) {
+		from := "testdata/out_offset6000_limit1000.txt"
+		var offset int64 = 6000
+		var limit int64 = 1000
+		err := Copy(from, ToFile, offset, limit)
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	tests1 := []struct {
+		title string
+		limit int64
+	}{
+		{title: "out_offset0_limit10.txt", limit: 10},
+		{title: "out_offset0_limit1000.txt", limit: 1000},
+	}
+
+	for _, tc := range tests1 {
+		tc := tc
+		t.Run(tc.title, func(t *testing.T) {
+			from := "testdata/" + tc.title
+			var offset int64
+			err := Copy(from, ToFile, offset, tc.limit)
+			require.Nil(t, err)
+
+			toFile, _ := os.Stat(ToFile)
+			toSize := toFile.Size()
+
+			require.Equal(t, tc.limit, toSize)
+		})
+	}
+
+	tests2 := []struct {
+		title  string
+		offset int64
+		limit  int64
+	}{
+		{title: "out_offset100_limit1000.txt", offset: 100, limit: 1000},
+		{title: "out_offset900_limit1000.txt", offset: 900, limit: 1000},
+	}
+
+	for _, tc := range tests2 {
+		tc := tc
+		t.Run(tc.title, func(t *testing.T) {
+			from := "testdata/" + tc.title
+			err := Copy(from, ToFile, tc.offset, tc.limit)
+			require.Nil(t, err)
+
+			toFile, _ := os.Stat(ToFile)
+			toSize := toFile.Size()
+
+			require.Equal(t, tc.limit-tc.offset, toSize)
+		})
+	}
 }
